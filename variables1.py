29d9d23b143c762c156e6b159263d1ff5f8f6320
¨Jose <saCUM@users.noreply.github.com>
2023-04-06 14:39:03 -0400

    oal

---
+++ variables1.py
@@ -0,0 +1,18 @@
+
+# cuanto valen lo elaos de cien
+nombre= "wena po"
+name= "vieja culia maraca"
+
+print(name)
+edad= 100
+print(edad)
+print(nombre)
+print ("wena po, mi nombre es", name )
+
+# wena cabros del yutu
+nombre="pablo"
+
+print("hola mi nombre es",nombre)
+nombre1= input("¿cual es tu nombre?\n")
+#wuaaaaaaaa
+print("tu nombre es:",nombre1)
